4c740222186a5d32550e64bf1d3b7d6ff2103fc3
Netanel Sananes <netanelile@gmail.com>
2017-05-26 14:56:21 +0300

    Saved changes at Emulator and AppiumServer

--- src/AppiumServerJava.java
+++ src/AppiumServerJava.java
@@ -1,4 +1,44 @@
+import java.io.IOException;
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecuteResultHandler;
+import org.apache.commons.exec.DefaultExecutor;
 
 public class AppiumServerJava {
-
-}
+	
+	public void startServer() {
+		System.out.println("Start Appium Server");
+		CommandLine cmd = new CommandLine("C:\\Program Files (x86)\\Appium\\node.exe");
+		cmd.addArgument("C:\\Program Files (x86)\\Appium\\node_modules\\appium\\bin\\Appium.js");
+		cmd.addArgument("--address");
+		cmd.addArgument("127.0.0.1");
+		cmd.addArgument("--port");
+		cmd.addArgument("4723");
+		
+		DefaultExecuteResultHandler handler = new DefaultExecuteResultHandler();
+		DefaultExecutor executor = new DefaultExecutor();
+		executor.setExitValue(1);
+		try {
+			executor.execute(cmd, handler);
+			Thread.sleep(10000);
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void stopServer() {
+		System.out.println("Stop Appium Server");
+		Runtime runtime = Runtime.getRuntime();
+		try {
+			runtime.exec("taskkill /F /IM node.exe");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+//	public static void main(String[] args) {
+//		AppiumServerJava appiumServer = new AppiumServerJava();
+//		appiumServer.startServer();
+//		
+//		appiumServer.stopServer();
+	}
--- src/Emulator.java
+++ src/Emulator.java
@@ -1,4 +1,34 @@
 
+import java.io.IOException;
+ 
 public class Emulator {
-
+	
+	public void startEmulator() {
+		System.out.println("Start Emulator");
+		Runtime runtime = Runtime.getRuntime();
+		try {
+			runtime.exec("cmd.exe /c start cmd.exe /k \"C:\\ProgramData\\BlueStacksGameManager\\BlueStacks.exe");
+			Thread.sleep(10000);
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void stopEmulator() {
+		System.out.println("close Emulator");
+		Runtime runtime = Runtime.getRuntime();
+		try {
+			runtime.exec("taskkill /F /IM BlueStacks.exe");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+//	public static void main(String[] args) {
+//		Emulator emulator = new Emulator();
+//		
+//		emulator.startEmulator();
+// 
+//		emulator.stopEmulator();
+//	}
 }
